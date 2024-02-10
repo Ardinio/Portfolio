8a0c7bffdb74d322af43aff9c5ecced1b5a3b3d5
Ardi Grapci <ardiehnberggrapci@gmail.com>
2024-02-10 12:05:13 +0100

    Added intro for my website

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/navbar/navbar';
+import Intro from './components/intro/intro';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Intro/>
     </div>
   );
 }
---
+++ src/components/intro/intro.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './intro.css';
+import bg from '../../assets/profile.png';
+import btnImg from '../../assets/HireMe.png';
+import { Link } from 'react-scroll/';
+
+function Intro() {
+    return (
+        <section id="intro">
+            <div className="introContent">
+                <span className="hello">Hello,</span>
+                <span className="introText">I'm <span className="introName">Ardi</span> 
+                <br />System Developer and IT Technician </span>
+                <p className="introPara">
+                    I am a software developer with experience in creating backend applications <br /> and I am a skilled IT technician.
+                </p>
+                <Link> <button className="btn"> <img src={btnImg} alt="Hire me" className="btnImg"/>Hire Me </button> </Link>
+            </div>
+            <img src={bg} alt="Profile" className="bg" />
+        </section>
+    )
+}
+
+export default Intro
