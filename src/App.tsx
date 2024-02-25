ba2baa29f27029ec67b277d181c4e39fa71bce36
Ardi Grapci <ardiehnberggrapci@gmail.com>
2024-02-25 12:16:01 +0100

    Added footer an funtion to my navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Intro from './components/intro/intro';
 import Skills from './components/skills/skills';
 import Works from './components/works/works';
 import Contact from './components/contact/contact';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Skills />
       <Works />
       <Contact />
+      <Footer />
     </div>
   );
 }
