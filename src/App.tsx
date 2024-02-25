4c4bdd33e14fb92f7c3df8ebb10d7049963488c3
Ardi Grapci <ardiehnberggrapci@gmail.com>
2024-02-25 16:55:37 +0100

    Concact would not change to lower case

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Navbar from './components/navbar/navbar';
 import Intro from './components/intro/intro';
 import Skills from './components/skills/skills';
 import Works from './components/works/works';
-import Contact from './components/contact/contact';
+import Contact from './components/Contact/contact';
 import Footer from './components/footer/footer';
 
 function App() {
